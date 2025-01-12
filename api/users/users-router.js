2525bbb19127451469e2c16374703a4b9e6f6aa0
Josh Rime <joshrime1990@icloud.com>
2025-01-12 00:11:37 -0600

    validatePost

--- api/users/users-router.js
+++ api/users/users-router.js
@@ -1,5 +1,9 @@
 const express = require("express");
-const { validateUserId, validateUser, validatePost } = require("../middleware/middleware");
+const {
+  validateUserId,
+  validateUser,
+  validatePost,
+} = require("../middleware/middleware");
 
 // You will need `users-model.js` and `posts-model.js` both
 // The middleware functions also need to be required
@@ -9,7 +13,6 @@ const router = express.Router();
 router.get("/", (req, res) => {
   // RETURN AN ARRAY WITH ALL THE USERS
   console.log(req.user);
-  
 });
 
 router.get("/:id", validateUserId, (req, res) => {
@@ -18,11 +21,10 @@ router.get("/:id", validateUserId, (req, res) => {
   console.log(req.user);
 });
 
-router.post("/", validateUser,(req, res) => {
+router.post("/", validateUser, (req, res) => {
   // RETURN THE NEWLY CREATED USER OBJECT
   // this needs a middleware to check that the request body is valid
   console.log(req.name);
-  
 });
 
 router.put("/:id", validateUserId, validateUser, (req, res) => {
@@ -31,30 +33,26 @@ router.put("/:id", validateUserId, validateUser, (req, res) => {
   // and another middleware to check that the request body is valid
   console.log(req.user);
   console.log(req.name);
-  
-  
 });
 
 router.delete("/:id", validateUserId, (req, res) => {
   // RETURN THE FRESHLY DELETED USER OBJECT
   // this needs a middleware to verify user id
   console.log(req.user);
-  
 });
 
 router.get("/:id/posts", validateUserId, (req, res) => {
   // RETURN THE ARRAY OF USER POSTS
   // this needs a middleware to verify user id
   console.log(req.user);
-  
 });
 
-router.post("/:id/posts", validateUserId, validatePost,(req, res) => {
+router.post("/:id/posts", validateUserId, validatePost, (req, res) => {
   // RETURN THE NEWLY CREATED USER POST
   // this needs a middleware to verify user id
   // and another middleware to check that the request body is valid
   console.log(req.user);
-  
+  console.log(req.text);
 });
 
 // do not forget to export the router
